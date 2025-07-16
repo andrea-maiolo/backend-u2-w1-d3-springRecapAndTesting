7f133bc18f4a84224b84c14ac27d82e5b8f43703
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-07-16 15:09:50 +0200

    initial commit

---
+++ src/main/java/andreamaiolo/demo/SpringrecapApplication.java
@@ -0,0 +1,13 @@
+package andreamaiolo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrecapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrecapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andreamaiolo/demo/SpringrecapApplicationTests.java
@@ -0,0 +1,13 @@
+package andreamaiolo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrecapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
